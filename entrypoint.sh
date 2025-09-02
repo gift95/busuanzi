7c47167d08d71054c0dada0dbb6d0a65b8293cee
野生Bug饲养员 <yxliuchn95@163.com>
2025-09-02 09:47:43 +0800

    fix: 修正index.html中API服务器地址的替换逻辑
    原替换逻辑错误地将https://bsz.hnlyx.top替换为API_SERVER，现统一使用http://127.0.0.1:8080作为匹配模式

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [ ! -f "/app/expose/install.lock" ];then
   # busuanzi js API address
   if [ -n "$API_SERVER" ];then
     sed -i "s/http:\/\/127.0.0.1:8080/$API_SERVER/g" dist/busuanzi.js
-    sed -i "s/https:\/\/bsz.hnlyx.top/$API_SERVER/g" dist/index.html
+    sed -i "s/http:\/\/127.0.0.1:8080/$API_SERVER/g" dist/index.html
   fi
   mv dist /app/expose/dist
   mv config.yaml /app/expose/config.yaml
